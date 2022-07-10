dccf693d499196f12607ca7b98fabfb3483c1264
magicalsoup <amagicalsoup@gmail.com>
2022-07-09 21:43:16 -0400

    add logic to make the snake avoid walls and fix typo in main_logic.py

--- src/logic/main_logic.py
+++ src/logic/main_logic.py
@@ -38,7 +38,7 @@ def choose_move(data: dict) -> str:
     possible_moves = avoid_my_neck(my_body, possible_moves)
     possible_moves = avoid_other_snakes(my_body, data["board"]["snakes"])
     possible_moves = avoid_walls(my_head, possible_moves, data["board"])
-    possivle_moves = avoid_my_body(my_body, possible_moves)
+    possible_moves = avoid_my_body(my_body, possible_moves)
 
     # TODO: Step 4 - Find food.
     # Use information in `data` to seek out and find food.
