51472ed7e2c7e2a2f44996a06da5d972d44e9842
magicalsoup <amagicalsoup@gmail.com>
2022-07-09 22:05:01 -0400

    add logic to make the snake avoid other snakes and fix parameters for this function in main_logic.py

--- src/logic/avoid_other_snakes.py
+++ src/logic/avoid_other_snakes.py
@@ -1,4 +1,22 @@
 from typing import List
 
-def avoid_other_snakes(my_body: dict, other_snakes: List[dict]) -> List[str]:
-    return ['left', 'right', 'up', 'down']
+
+def avoid_other_snakes(my_body: dict, possible_moves: List[str], other_snakes: List[dict]) -> List[str]:
+    new_moves_delta = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+    new_moves_name = ["up", "down", "right", "left"]
+    my_head = my_body[0]
+
+    for snake in other_snakes:
+        for bodyComponent in snake["body"]:
+
+            if len(possible_moves) == 0:
+                return possible_moves
+
+            for i in range(len(new_moves_delta)):
+                move_name, dx, dy = new_moves_name[i], new_moves_delta[i][0], new_moves_delta[i][1]
+                nx, ny = my_head["x"] + dx, my_head["y"] + dy
+                if move_name in possible_moves and (bodyComponent["x"] == nx and bodyComponent["y"]):
+                    possible_moves.remove(move_name)
+
+    return possible_moves
+
